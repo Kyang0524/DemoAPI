aef35cd969126a77e3887d872f97a5c3ebaac589
Ky <xiaoyang7481@gmail.com>
2023-09-04 14:17:17 +0800

    加入專案檔案。

---
+++ DemoAPI/Controllers/DemoApiController.cs
@@ -0,0 +1,69 @@
+﻿using DemoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWeb.Controllers
+{
+    public class DemoApiController : JsonNetController
+    {
+        [HttpPost]
+        public ActionResult PostString(ModelParameter InputText)
+        {
+            try
+            {
+
+                SecurityManager.Authorize(Request);//檢測IP是否允許連線，設定在Web.config。
+                return Json(new ApiSucc("1111",
+                    CodecModule.PostString(InputText.Name)));
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiError("500", ex.Message));
+            }
+        }
+
+        public class ModelParameter
+        {
+            public string Name { get; set; }
+        }
+
+
+        [HttpGet]
+
+        public ActionResult GetStr()
+        {
+            try
+            {
+                SecurityManager.Authorize(Request);//檢測IP是否允許連線，設定在Web.config。
+                string Text = "RT_LAB DemoWebApi";
+                return Json(new ApiSucc("1112",
+                    CodecModule.GetStr(Text)), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiError("500", ex.Message));
+            }
+        }
+
+        [HttpGet]
+
+        public ActionResult GetStrNChange(string inputText)
+        {
+            try
+            {
+                SecurityManager.Authorize(Request);//檢測IP是否允許連線，設定在Web.config。
+                return Json(new ApiSucc("1113",
+                    CodecModule.GetStrNChange(inputText)), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiError("500", ex.Message));
+            }
+        }
+
+    }
+}
---
+++ DemoAPI/Models/CodecModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    public class CodecModule
+    {
+
+        public static string PostString(string InputText)
+        {
+            string AfterPost = "Hello!" + InputText;
+            return AfterPost;
+        }
+
+        public static string GetStr(string Name)
+        {
+             return Name;
+
+        }
+        public static string GetStrNChange(string InputText)
+        {
+            string Text = "RT_LAB DemoWebApi " + InputText;
+            return Text;
+        }
+
+
+    }
+}
